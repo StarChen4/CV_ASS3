284a2dc6f925f43b881ad800d77505848aca8af6
Xing Chen <u7725171@anu.edu.au>
2024-05-17 15:19:06 +1000

    Task1 done. Undetected circles still.

--- Task1/HoughCircleTransform.py
+++ Task1/HoughCircleTransform.py
@@ -5,171 +5,161 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 
 
-class HoughCircleTransform:
-    def __init__(self, img_path):
-        """
-        ======================  task 1.1 =========================
-        Use cv2 to read and convert to gray scale
-        """
-        self.img_path = img_path
-        self.img_bgr = cv2.imread(self.img_path)
-        self.img_gray = cv2.cvtColor(self.img_bgr, cv2.COLOR_BGR2GRAY)
+def draw_circles(image, circles):
+    """
+    Draw circles on an image copy with green color
+    """
+    # Convert BGR image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Convert grayscale image back to BGR to draw colored circles
+    output_image = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
+    for circle in circles:
+        a, b, r = circle
+        cv2.circle(output_image, (b, a), r, (0, 255, 0), 2)
+    return output_image
 
-    def canny_edge_detect(self, canny_low, canny_high):
-        """
-        ======================  task 1.2 =========================
-        Apply canny edge detection on image and show the result
-        :param canny_low: Lower threshold of canny detection
-        :param canny_high: Higher threshold of canny detection
 
-        :return: a nparray image containing edges
-        """
-        # apply gaussian blur
-        img_gray = cv2.GaussianBlur(self.img_gray, (3,3),0)
-        img_edges = cv2.Canny(self.img_gray, canny_low, canny_high)
-        # show the original image and the detection result
-        plt.figure(figsize=(10, 5))
-        plt.subplot(121), plt.imshow(cv2.cvtColor(self.img_bgr, cv2.COLOR_BGR2RGB)), plt.title('Original Image')
-        plt.subplot(122), plt.imshow(img_edges, cmap='gray'), plt.title('Edge Image')
-        plt.show()
-        return img_edges
+def show_image(img_bgr, img_compare, title):
+    # using plt so show the image before and after processing
+    plt.figure(figsize=(10, 5))
+    plt.title(title)
+    plt.subplot(121), plt.imshow(cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)), plt.title('Original Image')
+    plt.subplot(122), plt.imshow(cv2.cvtColor(img_compare, cv2.COLOR_BGR2RGB)), plt.title('Edge Image')
+    plt.show()
 
-    def hough(self, edges, min_radius, max_radius, radius_step, theta_step, threshold_ratio):
-        """
-        ======================  task 1.3 =========================
-        Apply hough circle transform on image and show the result
-        :param edges: edges img for transformation
-        :param min_radius: Minimum radius of the circles to be detected
-        :param max_radius: Maximum radius of the circles to be detected
-        :param radius_step: Increment step for the radius
-        :param theta_step: Increment step for the angle theta (in degrees)
-        :param threshold_ratio: Ratio of the minimum score required to be considered a circle
 
-        :return: A list of detected circles represented as [a, b, r],
-        where (a, b) is the center and r is the radius.
-        """
-        print("Executing hough circle transform detection with threshold ratio = " + str(threshold_ratio))
-        # Get the dimensions of the input image
-        rows, cols = edges.shape[:2]
+def hough_circle_transform(edges, min_radius, max_radius, radius_step, angle_step, threshold_ratio):
+    """
+    Task1.3
+    Apply hough circle transformation on an edge image.
+    """
+    # Get the dimensions of the input image
+    height, width = edges.shape
 
-        # Initialize the accumulator array
-        accumulator = np.zeros((rows, cols, (max_radius - min_radius) // radius_step + 1))
+    # Initialize the accumulator array
+    num_radii = (max_radius - min_radius) // radius_step + 1  # Calculate number of radius
+    accumulator = np.zeros((height, width, num_radii), dtype=np.uint64)
 
-        # Get the indices of edge pixels
-        edge_pixels = np.argwhere(edges != 0)
+    # Get the indices of edge pixels
+    edge_pixels = np.argwhere(edges != 0)
 
-        # Iterate over the edge pixels
-        for ep in tqdm(edge_pixels):
-            x, y = ep
+    # precompute angles and cos, sin
+    angles = np.arange(0, 360, angle_step)
+    cos_theta = np.cos(np.deg2rad(angles))
+    sin_theta = np.sin(np.deg2rad(angles))
 
-            # Iterate over the possible radii
-            for r in range(min_radius, max_radius + 1, radius_step):
-                # Iterate over the possible theta values
-                for theta in range(0, 360, theta_step):
-                    # convert degree into rad
-                    theta_rad = np.deg2rad(theta)
+    # Iterate over all edge pixels and radius
+    for x, y in tqdm(edge_pixels, desc="Processing edge pixels", total=len(edge_pixels)):
+        for radius_index, radius in enumerate(range(min_radius, max_radius, radius_step)):
+            for cos_t, sin_t in zip(cos_theta, sin_theta):
+                a = int(x - radius * cos_t)  # Calculating a using formula a = x − r cos θ
+                b = int(y - radius * sin_t)  # Calculating b using formula b = y − r sin θ
+                # If within this image range, add the score by 1
+                if 0 <= a < height and 0 <= b < width:
+                    accumulator[a, b, radius_index] += 1
 
-                    # Calculate the center coordinates
-                    a = int(x - r * np.cos(theta_rad))
-                    b = int(y - r * np.sin(theta_rad))
+    # Find the maxima in the accumulator using threshold ratio
+    max_acc = np.max(accumulator)
+    threshold = threshold_ratio * max_acc
+    circles = []  # List to save the result
 
-                    # Increment the accumulator if the center is within the image bounds
-                    if 0 <= a < rows and 0 <= b < cols:
-                        accumulator[a, b, (r - min_radius) // radius_step] += 1
+    # Apply thresholding
+    for r in range(accumulator.shape[2]):
+        acc_slice = accumulator[:, :, r]  # get a slice of the accumulator
+        circle_indices = np.argwhere(acc_slice >= threshold)  # get the indices of circles with score is over threshold
+        for idx in circle_indices:
+            a, b = idx
+            circles.append([a, b, min_radius + r * radius_step])  # append the circle into result List
 
-        # Find the local maxima in the accumulator
-        max_score = np.max(accumulator)
-        threshold = max_score * threshold_ratio
-        circles = []
-        for r in range(accumulator.shape[2]):
-            acc_slice = accumulator[:, :, r]
-            circle_indices = np.argwhere(acc_slice >= threshold)
-            for idx in circle_indices:
-                a, b = idx
-                circles.append([a, b, min_radius + r * radius_step])
+    # Sort by descending order of the vote score of each circle
+    circles = sorted(circles, key=lambda x: accumulator[x[0], x[1], (x[2] - min_radius) // radius_step], reverse=True)
 
-        # Sort the circles based on the number of votes (accumulator values)
-        circles = sorted(circles, key=lambda x: accumulator[x[0], x[1], (x[2] - min_radius) // radius_step],
-                         reverse=True)
-        # Print the maximum score and the number of circles to be returned
-        print(f"Maximum score: {max_score}")
-        print(f"Number of circles to be returned: {len(circles)}")
+    return circles
 
-        # draw circles on the original images
-        img_bgr_copy = self.img_bgr.copy()  # get a copy of original image
-        for circle in circles:
-            a, b, r = circle
-            cv2.circle(img_bgr_copy, (b, a), r, (0, 255, 0), 1)  # green color
-        # Display the result
-        cv2.imshow('Detected Circles', img_bgr_copy)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
-        return circles, accumulator
+def non_maximum_suppression(circles, threshold_pixels):
+    """
+    Task 1.4
+    Apply nms on hough circle detection result
+    """
+    # To save result circles
+    suppressed_circles = []
 
-    def nms_hough(self, edges, min_radius, max_radius, radius_step, theta_step, threshold_ratio, nms_threshold):
-        """
-        ======================  task 1.4 =========================
-        Apply non-maximum suppression on the result of hough transformation
-        :param circles: List of detected circles, each represented as [a, b, r] where (a, b) is the center and r is the radius.
-        :param threshold: Threshold value for suppressing similar circles.
+    # A marker to mark circles that are suppressed
+    suppressed = np.zeros(len(circles), dtype=bool)
 
-        :return: A list of circles after non-maximum suppression.
-        """
-        circles, accumulator = self.hough(edges, min_radius, max_radius, radius_step, theta_step, threshold_ratio)
-        print("Applying Non Maximum Suppression with threshold = " + str(nms_threshold))
-        # If there are no circles, return an empty list
-        if len(circles) == 0:
-            return []
+    for i, (a1, b1, r1) in tqdm(enumerate(circles)):
+        # skip suppressed circles
+        if suppressed[i]:
+            continue
 
-        # Convert circles to numpy array for easier indexing
-        circles = np.array(circles)
-
-        # Get the scores (accumulator values) for each circle
-        scores = np.array([accumulator[int(a), int(b), int((r - min_radius) / radius_step)] for a, b, r in circles])
-
-        # Sort the circles based on their scores in descending order
-        order = np.argsort(scores)[::-1]
-        circles = circles[order]
-        scores = scores[order]
-
-        # Initialize an array to keep track of suppressed circles
-        suppressed = np.zeros(len(circles), dtype=bool)
-
-        # Iterate over the circles
-        for i in range(len(circles)):
-            if suppressed[i]:
+        for j, (a2, b2, r2) in enumerate(circles[i+1:], start=i+1):  # Other circles
+            # skip suppressed circles
+            if suppressed[j]:
                 continue
 
-            # Compare the current circle with the remaining circles
-            for j in range(i+1, len(circles)):
-                if suppressed[j]:
-                    continue
+            # Compare the difference of a, b and r. If below threshold, suppress it.
+            if abs(a1 - a2) <= threshold_pixels and abs(b1 - b2) <= threshold_pixels and abs(r1 - r2) <= threshold_pixels:
+                suppressed[j] = True
 
-                # Calculate the difference in center coordinates and radii
-                da = circles[i, 0] - circles[j, 0]
-                db = circles[i, 1] - circles[j, 1]
-                dr = circles[i, 2] - circles[j, 2]
+        if not suppressed[i]:
+            suppressed_circles.append((a1, b1, r1))
 
-                # If the differences are smaller than the threshold, suppress the circle with lower score
-                if abs(da) < nms_threshold and abs(db) < nms_threshold and abs(dr) < nms_threshold:
-                # if abs(da) < nms_threshold and abs(db) < nms_threshold and abs(dr) < nms_threshold:
-                    suppressed[j] = True
+    return suppressed_circles
 
-        print(f"Number of circles after suppression: {len(circles[~suppressed])}")
-        # Draw the result on original image
-        img_bgr_copy = self.img_bgr.copy()  # get a copy of original image
-        for circle in circles[~suppressed]:
-            a, b, r = circle
-            cv2.circle(img_bgr_copy, (b, a), r, (0, 255, 0), 1)  # green color
-        # Display the result
-        cv2.imshow('Detected Circles after NMS', img_bgr_copy)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
-        # Return the circles that were not suppressed
-        return circles[~suppressed]
+def hough_circle_transform_outside_image(edges, min_radius, max_radius, radius_step, angle_step, threshold_ratio):
+    """
+    Task1.3
+    Apply hough circle transformation on an edge image.
+    """
+    # Get the dimensions of the input image
+    height, width = edges.shape
 
+    # Initialize the accumulator array
+    max_offset = max_radius  #
+    acc_height = height + 2 * max_offset
+    acc_width = width + 2 * max_offset
+    num_radii = (max_radius - min_radius) // radius_step + 1  # Calculate number of radius
+    accumulator = np.zeros((acc_height, acc_width, num_radii), dtype=np.uint64)
+
+    # Get the indices of edge pixels
+    edge_pixels = np.argwhere(edges != 0)
+
+    # precompute angles and cos, sin
+    angles = np.arange(0, 360, angle_step)
+    cos_theta = np.cos(np.deg2rad(angles))
+    sin_theta = np.sin(np.deg2rad(angles))
+
+    # Iterate over all edge pixels and radius
+    for x, y in tqdm(edge_pixels, desc="Processing edge pixels", total=len(edge_pixels)):
+        for radius_index, radius in enumerate(range(min_radius, max_radius, radius_step)):
+            for cos_t, sin_t in zip(cos_theta, sin_theta):
+                a = int(x - radius * cos_t)  # Calculating a using formula a = x − r cos θ
+                b = int(y - radius * sin_t)  # Calculating b using formula b = y − r sin θ
+                # If within the extended accumulator array
+                if 0 <= a < acc_height and 0 <= b < acc_width:
+                    accumulator[a, b, radius_index] += 1
+
+    # Find the maxima in the accumulator using threshold ratio
+    max_acc = np.max(accumulator)
+    threshold = threshold_ratio * max_acc
+    circles = []  # List to save the result
+
+    # Apply thresholding
+    for r in range(accumulator.shape[2]):
+        acc_slice = accumulator[:, :, r]  # get a slice of the accumulator
+        circle_indices = np.argwhere(acc_slice >= threshold)  # get the indices of circles with score is over threshold
+        for idx in circle_indices:
+            a, b = idx
+            a -= max_offset  # adjust the offset to go back to original coordinates
+            b -= max_offset  # adjust the offset to go back to original coordinates
+            circles.append([a, b, min_radius + r * radius_step])  # append the circle into result List
+
+    # Sort by descending order of the vote score of each circle
+    circles = sorted(circles, key=lambda x: accumulator[x[0], x[1], (x[2] - min_radius) // radius_step], reverse=True)
+
+    return circles
 
 
 
--- Task1/Task1.py
+++ Task1/Task1.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-from Task1.HoughCircleTransform_adv import hough_circle_transform, draw_circles, non_maximum_suppression, show_image
+from Task1.HoughCircleTransform import hough_circle_transform, draw_circles, non_maximum_suppression, show_image, hough_circle_transform_outside_image
 
 
 """
@@ -30,15 +30,15 @@ image_smarties_edges = cv2.Canny(image_smarties_gray, 30, 65)  # use canny edge 
 # ========================  Task 1.3 ===========================
 # apply hough circle transformation on images to get circles
 print("Hough circle transformation: processing coins.png")
-coins_circles = hough_circle_transform(image_coins_edges, 10, 100, 1, 2, 0.75)  # 40,70,1,1,0.6
+coins_circles = hough_circle_transform(image_coins_edges, 20, 50, 2, 10, 0.7)  # 20,50,2,10,0.7
 print("Hough circle transformation: processing smarties.png")
-smarties_circles = hough_circle_transform(image_smarties_edges, 10, 100, 1, 2, 0.75)  # 40,60,1,1,0.6
+smarties_circles = hough_circle_transform(image_smarties_edges, 20, 50, 2, 10, 0.6)  # 20,50,2,10,0.6
 # draw circles on original images
 image_coins_hough = draw_circles(image_coins_bgr, coins_circles)
 image_smarties_hough = draw_circles(image_smarties_bgr, smarties_circles)
 # save result images as [image_name]_hough_result.jpg
-cv2.imwrite("coins_hough_result.jpg", image_coins_hough)
-cv2.imwrite("smarties_hough_result.jpg", image_smarties_hough)
+cv2.imwrite("results/coins_hough_result.jpg", image_coins_hough)
+cv2.imwrite("results/smarties_hough_result.jpg", image_smarties_hough)
 # plot images after hough
 show_image(image_coins_bgr, image_coins_hough, "Hough Circle Transform")
 show_image(image_smarties_bgr, image_smarties_hough, "Hough Circle Transform")
@@ -46,18 +46,38 @@ show_image(image_smarties_bgr, image_smarties_hough, "Hough Circle Transform")
 # ========================  Task 1.4 ===========================
 # apply nms on hough result
 print("NMS: processing coins.png")
-coins_circles_nms = non_maximum_suppression(coins_circles, 10)
+coins_circles_nms = non_maximum_suppression(coins_circles, 20)
 print("NMS: processing smarties.png")
 smarties_circles_nms = non_maximum_suppression(smarties_circles, 10)
 # draw nms hough circles on original images
 image_coins_hough_nms = draw_circles(image_coins_bgr, coins_circles_nms)
 image_smarties_hough_nms = draw_circles(image_smarties_bgr, smarties_circles_nms)
-# save result images as [image_name]_hough_result.jpg
-cv2.imwrite("coins_hough_nms_result.jpg", image_coins_hough_nms)
-cv2.imwrite("smarties_hough_nms_result.jpg", image_smarties_hough_nms)
+# save result images as [image_name]_hough_nms_result.jpg
+cv2.imwrite("results/coins_hough_nms_result.jpg", image_coins_hough_nms)
+cv2.imwrite("results/smarties_hough_nms_result.jpg", image_smarties_hough_nms)
 # plot images after nms
 show_image(image_coins_bgr, image_coins_hough_nms, "Hough Circle Transform with NMS")
 show_image(image_smarties_bgr, image_smarties_hough_nms, "Hough Circle Transform with NMS")
+
+# # ========================  Task 1.6 ===========================
+# # detect circles even if they are outside the image range
+# print("Hough circle transformation: processing coins.png")
+# coins_circles_contain_outside = hough_circle_transform_outside_image(image_coins_edges, 25, 50, 2, 10, 0.7)
+# print("Hough circle transformation: processing smarties.png")
+# smarties_circles_contain_outside = hough_circle_transform_outside_image(image_smarties_edges, 25, 50, 2, 10, 0.6)
+# print("NMS: processing coins.png")
+# coins_circles_nms_outside = non_maximum_suppression(coins_circles_contain_outside, 10)
+# print("NMS: processing smarties.png")
+# smarties_circles_nms_outside = non_maximum_suppression(smarties_circles_contain_outside, 10)
+# # draw nms hough circles on original images
+# image_coins_hough_nms_outside = draw_circles(image_coins_bgr, coins_circles_nms_outside)
+# image_smarties_hough_nms_outside = draw_circles(image_smarties_bgr, smarties_circles_nms_outside)
+# # save result images as [image_name]_hough_nms_outside_result.jpg
+# cv2.imwrite("results/coins_hough_nms_outside_result.jpg", image_coins_hough_nms_outside)
+# cv2.imwrite("results/smarties_hough_nms_outside_result.jpg", image_smarties_hough_nms_outside)
+# # plot images after nms
+# show_image(image_coins_bgr, image_coins_hough_nms_outside, "Hough Circle Transform with NMS")
+# show_image(image_smarties_bgr, image_smarties_hough_nms_outside, "Hough Circle Transform with NMS")
 
 
 
